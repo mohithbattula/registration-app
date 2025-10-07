52d491c2ceb1ae87c2cdf298677a4b7e9c77b32b
Subha Moneesh Kasireddi <subhamoneeshkasireddi@Subhas-MacBook-Air.local>
2025-10-08 02:30:30 +0530

    added User Entity, UserRepository interface, controller method and result page

--- src/main/java/com/registration/example/registration/controllers/RegistrationController.java
+++ src/main/java/com/registration/example/registration/controllers/RegistrationController.java
@@ -1,9 +1,22 @@
 package com.registration.example.registration.controllers;
 
+import org.springframework.ui.Model;
+import com.registration.example.registration.models.User;
+import com.registration.example.registration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @RestController
 public class RegistrationController {
@@ -14,8 +27,13 @@ public class RegistrationController {
         return view;
     }
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/register")
-    public String register() {
-        return "Successfully Registered!";
+    public String register(@ModelAttribute User user, Model model) {
+        userRepository.save(user);
+        model.addAttribute("user", user);
+        return "result";
     }
 }
